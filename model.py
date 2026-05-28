2bc870e421131a532080750ef09d2e93d12ebde8
Ishan Madusanka <ishancoc295@gmail.com>
2026-05-28 23:20:42 +0530

    Refactor model.py to consolidate data normalization and model definition

--- model.py
+++ model.py
@@ -6,7 +6,15 @@ models = keras.models
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+model = models.Sequential([
+    layers.Flatten(input_shape = (28, 28)),
+
+    layers.Dense(128, activation='relu'),
+
+    layers.Dense(10, activation = 'softmax')
+])
+
+model.summary()
 
